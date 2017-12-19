44b44bc24bf7c1ace13ba9a297f3e134f4ecd385
Patricia Wong <patriciawong08@gmail.com>
2017-12-19 18:46:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string)
+{
+  return string.toUpperCase()
+}
+
+function whisper(string)
+{
+  return string.toLowerCase()
+}
+
+function logShout(string)
+{
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string)
+{
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string)
+{
+  var uppercase = "HELLO!"
+  var lowercase = "hello!"
+  
+  if (string.toUpperCase() === uppercase) 
+  {
+    return "YES INDEED!"
+  }
+  else if (string.toLowerCase() === lowercase)
+  {
+    return "I can't hear you!"
+  }
+}
